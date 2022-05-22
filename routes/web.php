79326c3e82c87e93ee8f68b7fe16b34b6d4919e2
EC2 Default User <ec2-user@ip-172-31-24-80.ec2.internal>
2022-05-22 10:57:07 +0000

    非ログイントップページ作成

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +15,11 @@ use App\Http\Controllers\WelcomeController;
 |
 */
 
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
+// タスク管理システム
+Route::get('/', [AuthController::class, 'index']);
+
+
+// テスト用
 Route::get('/welcome', [\App\Http\Controllers\WelcomeController::class, 'index']);
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
 
